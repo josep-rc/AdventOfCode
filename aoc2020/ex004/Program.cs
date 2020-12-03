a5ab0e4695895743f2e621c65450bf82d1406b3d
Josep Peñalba <jopema7@gmail.com>
2020-12-03 22:28:29 +0100

    Ready for day 4

---
+++ aoc2020/ex004/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ex004
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Star1.Run();
+            Star2.Run();
+        }
+    }
+}
---
+++ aoc2020/ex004/Star1.cs
@@ -0,0 +1,10 @@
+namespace ex004
+{
+    public static class Star1
+    {
+        public static void Run()
+        {
+            
+        }
+    }
+}
